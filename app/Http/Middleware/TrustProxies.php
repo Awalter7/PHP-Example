d707063acfcbdfbadb8170322e70ab090e98ae43
Awalter7 <89409100+Awalter7@users.noreply.github.com>
2025-05-21 15:19:51 -0400

    Added TrustProxies.php

---
+++ app/Http/Middleware/TrustProxies.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Illuminate\Http\Middleware\TrustProxies as Middleware;
+use Illuminate\Http\Request;
+
+class TrustProxies extends Middleware
+{
+    /** @var array|string|null */
+    protected $proxies = '*';
+
+    /** @var int */
+    protected $headers = Request::HEADER_X_FORWARDED_ALL;
+}
